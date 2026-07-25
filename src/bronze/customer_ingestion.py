d4a0befc874b184ef87a592768cd91e7c3998cfd
Sekhar0146 <sekhar.0146@gmail.com>
2026-07-25 23:58:34 +0530

    add ingestion file

---
+++ src/bronze/customer_ingestion.py
@@ -0,0 +1,23 @@
+from src.common.config import load_config
+from src.common.logger import get_logger
+
+def main():
+
+    config = load_config("configs/dev.yml")
+
+    logger = get_logger(__name__)
+
+    catalog = config["catalog"]
+    bronze_schema = config["schemas"]["bronze"]
+    volume_path = config["volumes"]["landing"]
+    customer_table = config["tables"]["customer"]
+
+    logger.info(f"Catalog       : {catalog}")
+    logger.info(f"Bronze Schema : {bronze_schema}")
+    logger.info(f"Volume        : {volume_path}")
+    logger.info(f"Table         : {customer_table}")
+
+    logger.info("Customer ingestion started")
+
+if __name__ == "__main__":
+    main()
